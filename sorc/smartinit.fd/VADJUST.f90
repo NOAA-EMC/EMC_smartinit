e239d6840bc90b8448cc11c98c001da2d6e51bbb
jeff.mcqueen@noaa.gov <jeff.mcqueen@noaa.gov>
2014-05-02 21:45:27 +0000

    05/02/2014 * sorc/smartinit.fd/VADJUST.f90   - Restricted u,v adjustment if less than 10 m/s and   - all 8 surrounding points are valid.   - also removed validpt check when solving Poisson equation for velocity potential   - also set PHI = 0.1 over not validpts Produces a larger impact

--- sorc/smartinit.fd/VADJUST.f90
+++ sorc/smartinit.fd/VADJUST.f90
@@ -43,12 +43,11 @@
       ALLOCATE (PHI(NX,NY,2),STAT=kret)
 
 !     COMPUTE TERRAIN GRADIENTS AND INITIAL POTENTIAL
-
+      UB=0.1;VB=0.1
       DXI=0.5/DX
       DYI=0.5/DY
       do j=1,ny
       do i=1,nx
-        if (validpt(i,j)) then
          HBAR=HGHT(I,J,1)
          FX=DXI/(HBAR)
          FY=DYI/(HBAR)
@@ -62,21 +61,31 @@
          IF(J.LT.NY) HTOJP1=HTOPO(I,J+1)
          DHDX=(HTOIP1-HTOIM1)*FX
          DHDY=(HTOJP1-HTOJM1)*FY
-         PHI(I,J,2)=(U(I,J)*DHDX+V(I,J)*DHDY)
+         if (validpt(i,j)) then 
+           PHI(I,J,2)=(U(I,J)*DHDX+V(I,J)*DHDY)
 
 !     CALCULATE THE VERTICAL VELOCITY DUE TO TOPOGRAPHIC EFFECTS (JTM)
-         WTOPO=U(I,J)*DHDX+V(I,J)*DHDY
-         PHI(I,J,1)=WTOPO
+           WTOPO=U(I,J)*DHDX+V(I,J)*DHDY
+           PHI(I,J,1)=WTOPO
+!          TEST assume terrain following winds
+           PHI(I,J,1)=0.1
+         else
+          PHI(I,J,1)=0.1
+          PHI(I,J,2)=0.1
         endif
       enddo
       enddo
 
 !     SET BOUNDARY VALUES FOR PHI
-
       UB(:,1)=PHI(1,:,1)
       UB(:,2)=PHI(NX,:,1)
       VB(:,1)=PHI(:,1,1)
       VB(:,2)=PHI(:,NY,1)
+
+      print *,'VADJUST PHI 1 :', MINVAL(PHI(:,:,1)),MAXVAL(PHI(:,:,1))
+      print *,'VADJUST PHI 2 :', MINVAL(PHI(:,:,2)),MAXVAL(PHI(:,:,2))
+      print *,'VADJUST UB  :', MINVAL(UB(:,1)),MAXVAL(UB(:,2))
+      print *,'VADJUST VB  :', MINVAL(VB(:,1)),MAXVAL(VB(:,2))
 
 !     SOLVE POISSON EQUATION BY GAUSS-SEIDEL METHOD FOR
 !     VELOCITY POTENTIAL
@@ -85,13 +94,12 @@
       DYSQ=DY*DY
       DSQ=DXSQ*DYSQ
       FACT=1.0/(2.0*(DXSQ+DYSQ))
-      print *,'VADJUST DX  DY  FACT', DX,DY,FACT
+      print *,'VADJUST DX  DY ', DX,DY,' FACT', FACT
       DO 100 IT=1,ITMAX
         DO 90 IDIR=1,4
           ERROR=-1.0E+09
           do jj=1,ny
           do ii=1,nx
-          if (validpt(i,j)) then
             SELECT CASE (IDIR)
              CASE (1)
               I=II
@@ -113,7 +121,7 @@
            PHIJM1=VB(I,1)
            IF(J.GT.1)PHIJM1=PHI(I,J-1,KK)
            PHIIP1=UB(J,2)
-           IF(I.LT.NX)PHIIP1=PHI(I+1,J,KK)
+           IF(I.LT.NX )PHIIP1=PHI(I+1,J,KK)
            PHIJP1=VB(I,2)
            IF(J.LT.NY)PHIJP1=PHI(I,J+1,KK)
            XX=DYSQ*(PHIIP1+PHIIM1)
@@ -128,18 +136,22 @@
            IF(ABS(XOLD).GE.1.0E-10) THEN     
              ERR=ABS((PHI(I,J,KK)-XOLD)/XOLD)
              ERROR=AMAX1(ERR,ERROR)
-           ENDIF 
-          endif
+           ENDIF
          enddo
          enddo 
    90   CONTINUE
+        print *,'VADJUST :  ERROR',IT, IDIR, ERROR,' EPSI',EPSI,' XOLD',PHIOLD
         IF (ERROR.LE.EPSI) exit
   100 CONTINUE
 
+      print *,'VADJUST PHI 1 :POIS ', MINVAL(PHI(:,:,1)),MAXVAL(PHI(:,:,1))
+      print *,'VADJUST PHI 2 :', MINVAL(PHI(:,:,2)),MAXVAL(PHI(:,:,2))
+      print *,'VADJUST UB  :', MINVAL(UB(:,1)),MAXVAL(UB(:,2))
+      print *,'VADJUST VB  :', MINVAL(VB(:,1)),MAXVAL(VB(:,2))
+
 !     COMPUTE WIND COMPONENTS FROM VELOCITY POTENTIAL
-      do j=1,ny
-      do i=1,nx
-       if (validpt(i,j)) then
+      do j=2,ny-1
+      do i=2,nx-1
         PHIIM1=UB(J,1)
         PHIJM1=VB(I,1)
         PHIIP1=UB(J,2)
@@ -148,17 +160,22 @@
         IF(J.GT.1) PHIJM1=PHI(I,J-1,KK)
         IF(I.LT.NX) PHIIP1=PHI(I+1,J,KK)
         IF(J.LT.NY) PHIJP1=PHI(I,J+1,KK)
-        UOLD=U(I,J)
-        VOLD=V(I,J)
-        U(I,J)=(PHIIP1-PHIIM1)*DXI+U(I,J)
-        V(I,J)=(PHIJP1-PHIJM1)*DYI+V(I,J)
-        diffi=UOLD-U(i,j)
-        diffj=VOLD-V(i,j)
-        if (abs(diffi).gt.0.1)  print *,i,j,'VADJ PHII',PHIIP1, PHIIM1,DXI,UOLD,U(I,J)
-        if (abs(diffj).gt.0.1)   print *,i,j,'VADJ PHIJ',PHIJP1, PHIJM1,DYI,VOLD,V(I,J)
+        if (validpt(i-1,j).and.validpt(i+1,j).and.validpt(i,j-1).and.validpt(i,j+1).and. &
+            validpt(i-1,j-1).and.validpt(i+1,j+1).and. validpt(i-1,j+1).and.  &
+            validpt(i+1,j-1).and.validpt(i,j)) then
+          UOLD=U(I,J)
+          VOLD=V(I,J)
+          U(I,J)=(PHIIP1-PHIIM1)*DXI+U(I,J)
+          V(I,J)=(PHIJP1-PHIJM1)*DYI+V(I,J)
+          diffi=UOLD-U(i,j)
+          diffj=VOLD-V(i,j)
+          if (abs(diffi).gt.10.) U(I,J)=UOLD
+          if (abs(diffj).gt.10.) V(I,J)=VOLD
        endif
       enddo
       enddo
+      print *,'VADJUST UWND  :',  MINVAL(U(:,:)),MAXVAL(U(:,:))
+      print *,'VADJUST VWND  :',  MINVAL(V(:,:)),MAXVAL(V(:,:))
 
 !     RESET BOUNDARY ARRAY
       UB=0.
