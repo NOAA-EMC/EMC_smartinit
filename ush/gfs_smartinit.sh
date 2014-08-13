52250c8c102ae5e72ead00eb568cc33171de731e
dana.carlis@noaa.gov <dana.carlis@noaa.gov>
2014-08-13 03:25:58 +0000

    Update location of ncep_post exec

--- ush/gfs_smartinit.sh
+++ ush/gfs_smartinit.sh
@@ -183,7 +183,7 @@ fi
 	    #Convert guassian grid flux file to lat/lon degree grid 
             export fgrid='255 0 1440 721 90000 0 128 -90000 359750 250 250 0'
             $utilexec/copygb -g "$fgrid" -x $COMIN/$mdl.t${cyc}z.sfluxgrbf$fhr $FLXINP
-            export POSTGPEXEC=${POSTGPEXEC:-/global/save/emc.glopara/svn/post/tags/post_upgrade_gfs_2014_v8/src/ncep_post}
+            export POSTGPEXEC=${POSTGPEXEC:-/global/save/emc.glopara/svn/post/tags/post_upgrade_gfs_2014_v8/exec/ncep_post}
 	    export POSTGPSH=${POSTGPSH:-/global/save/emc.glopara/svn/post/tags/post_upgrade_gfs_2014_v8/ush/global_nceppost.sh}
 	    $POSTGPSH > post${fhr}.out
             $utilexec/copygb -g "$grid" -x tmpfile4 $mdl.t${cyc}z.smartinitin${mdlgrd}.${fhr}
