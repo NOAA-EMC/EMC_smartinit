9e3adcffc54f467a9c809cf82a330ae2ef1b850c
jeff.mcqueen@noaa.gov <jeff.mcqueen@noaa.gov>
2014-11-07 21:53:03 +0000

    nwprod v3.2.7 with VADJUST.f90 corrections

---
+++ ush/dng_awp.sh
@@ -0,0 +1,85 @@
+#!/bin/ksh
+#=======================================================================
+# Script developed by NCO to :
+# bsm - add processing for conversion to grib2 and awips files
+# called by : smartinit.sh
+# INPUT:
+#  RGIN :  2 letter Region id (eg: CS, HI, PR,AK..)
+#  cyc  :  UTC run cycle
+#  fhr  :  Forecasts hour
+#  ogrd :  output NDFD grid number (eg: 197,196,195,198...)
+#  outreg  :  output file region name (eg: conus,ak,pr,hi,conus2p5,ak3
+#=======================================================================
+set -x
+outreg=$1    # mdlgrd used for guam to distinguish arw/nmm
+ihindex=0
+case $cyc in 
+  00|12) cyctp=on;;
+  06|18) cyctp=off;;
+esac
+
+REGCP=`echo $outreg |tr '[a-z]'  '[A-Z]' `
+echo BEGIN NCO sminit Post-Processing for REG $RGIN $outreg $ogrd CYC $cyc FHR $fhr 
+
+   
+# Create HAINES INDEX GRIB FILE
+if [ $ihindex -eq 1 ];then
+ ${utilexec}/wgrib  MESO${RGIN}${fhr}.tm00 |grep ":HINDEX" | \
+  ${utilexec}/wgrib -i -grib  MESO${RGIN}${fhr}.tm00 -o hindex.t${cyc}z.smart${outreg}${fhr}.tm00
+ $utilexec/cnvgrib -g12 -p40 hindex.t${cyc}z.smart${outreg}${fhr}.tm00  hindex.t${cyc}z.smart${outreg}${fhr}.tm00.grib2
+fi
+$utilexec/cnvgrib -g12 -p40 MESO${RGIN}${fhr}.tm00 ${mdl}.t${cyc}z.smart${outreg}${fhr}.tm00.grib2
+
+# Processing grids for AWIPS
+ pgm=tocgrib2
+ export pgm;  . prep_step
+ startmsg
+export FORTREPORTS=unit_vars=yes
+export FORT11=${mdl}.t${cyc}z.smart${outreg}${fhr}.tm00.grib2 
+export FORT31="";
+export FORT51=grib2.t${cyc}z.smart${outreg}f${fhr}
+
+# Define grib2 awips parm file 
+if [ $outreg = conus2p5 ];then
+  awpparm=$utilparm/grib2_awp${mdl}dngconus${cyctp}f${fhr}.${ogrd}
+elif [ $outreg = ak3 ];then
+  awpparm=$utilparm/grib2_awp${mdl}dngak${cyctp}f${fhr}.${ogrd}
+else
+  awpparm=$utilparm/grib2_awp${mdl}smart${outreg}${cyctp}f${fhr}.${ogrd}
+fi
+
+if [ -s "$awpparm" ];then
+  $utilexec/tocgrib2 < ${awpparm} 1 >> $pgmout 2>> errfile
+  echo " error from tocgrib="  $err
+else 
+  echo AWP PARM FILE not found: $awpparm
+fi
+
+mv MESO${RGIN}${fhr}.tm00 $COMOUT/${mdl}.t${cyc}z.smart${outreg}${fhr}.tm00
+mv ${mdl}.t${cyc}z.smart${outreg}${fhr}.tm00.grib2 $COMOUT/${mdl}.t${cyc}z.smart${outreg}${fhr}.tm00.grib2
+if [ $ihindex -eq 1 ];then
+ mv hindex.t${cyc}z.smart${outreg}${fhr}.tm00 $COMOUT/hindex.t${cyc}z.smart${outreg}${fhr}.tm00
+ mv hindex.t${cyc}z.smart${outreg}${fhr}.tm00.grib2 $COMOUT/hindex.t${cyc}z.smart${outreg}${fhr}.tm00.grib2
+fi
+
+# Move grib2 awips file to pcom
+if [ $outreg = ak3 ];then
+  mv grib2.t${cyc}z.smart${outreg}f${fhr} $pcom/grib2.awp${mdl}smart3.ak${fhr}_awips_f${fhr}_${cyc}
+else
+  mv grib2.t${cyc}z.smart${outreg}f${fhr} $pcom/grib2.awp${mdl}smart.${outreg}${fhr}_awips_f${fhr}_${cyc}
+fi
+
+if [ -s "$awpparm" ];then
+  if [ $SENDDBN = YES ];then #bsm 25 feb 2008 - added code for awips alerts
+    if [ $outreg = ak3 ];then
+      $DBNROOT/bin/dbn_alert NTC_LOW SMART${REGCP} $job $pcom/grib2.awp${mdl}smart3.ak${fhr}_awips_f${fhr}_${cyc}
+    else
+      $DBNROOT/bin/dbn_alert NTC_LOW SMART${REGCP} $job $pcom/grib2.awp${mdl}smart.${outreg}${fhr}_awips_f${fhr}_${cyc}
+    fi
+  fi
+fi
+
+export MDL=`echo $mdl |tr [a-z] [A-Z]`
+if [ $SENDDBN = YES ];then
+  $DBNROOT/bin/dbn_alert MODEL ${MDL}_SMART${REGCP}_GB2 $job $COMOUT/${mdl}.t${cyc}z.smart${outreg}${fhr}.tm00.grib2
+fi
