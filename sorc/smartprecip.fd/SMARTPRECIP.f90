fba1fc005685cad5a15ea1abb73d95a1d2ce02f7
annette.gibbs@noaa.gov <annette.gibbs@noaa.gov>
2015-05-28 19:23:28 +0000

    Added GRIB2 input/output code

--- sorc/smartprecip.fd/SMARTPRECIP.f90
+++ sorc/smartprecip.fd/SMARTPRECIP.f90
@@ -1,4 +1,7 @@
        PROGRAM SMARTPRECP
+
+        USE GRIB_MOD
+
 !                .      .    .                                       .
 ! SUBPROGRAM:    SMARTPECIP
 !   PRGMMR: MANIKIN        ORG: W/NP22     DATE:  07-03-07
@@ -9,6 +12,7 @@
 ! PROGRAM HISTORY LOG:
 !   07-03-07  GEOFF MANIKIN 
 !   10-25-12  JEFF MCQUEEN
+!   05-16-15  ANNETTE GIBBS
 ! REMARKS:
 !   10-25-12 JTM UNIFIED make and add precip for different accum hours
 !                addprecip6, addprecip12 and makeprecip all combined in
@@ -27,7 +31,9 @@
 !               fhr1=fhr6 6hr precip
 !               fhr2=fhr  6hr precip
 !                 12hrp=fhr6 + fhr
-
+!  
+! 05-16-15 AMG Added GRIB2 code and option to choose GRIB1 or GRIB2 input/output
+!
 ! ATTRIBUTES:
 !   LANGUAGE: FORTRAN-90
 !   MACHINE:  WCOSS     
@@ -36,6 +42,18 @@
       INTEGER SHR1,FHR1, FHR2, FHR3, FHR4
       CHARACTER*80 FNAME
       LOGICAL*1 LSUB, MK3P,MK6P,MK12P,LADDSUB,LRD3,LRD4
+      character*1 igrbin, igrbout
+
+!C grib2
+      INTEGER :: LUGB,LUGI,J,JDISC,JPDTN,JGDTN
+      INTEGER,DIMENSION(:) :: JIDS(200),JPDT(200),JGDT(200)
+      INTEGER,DIMENSION(:) :: PDS_SNOW_HOLD(200),PDS_RAIN_HOLD(200)
+      INTEGER,DIMENSION(:) :: PDS_SNOW_HOLD_EARLY(200), &
+                              PDS_RAIN_HOLD_EARLY(200)
+      LOGICAL :: UNPACK
+      INTEGER :: K,IRET
+      TYPE(GRIBFIELD) :: GFLD
+!C grib2
 
       REAL,     ALLOCATABLE :: GRID(:)
       REAL,     ALLOCATABLE :: APCP1(:),APCP2(:),APCP3(:),APCP4(:)
@@ -63,6 +81,12 @@
       END INTERFACE
 !--------------------------------------------------------------------------
 
+      call get_environment_variable("IGRBI",igrbin)  ! GRIB input: 1=GRIB1; 2=GRIB2
+      call get_environment_variable("IGRBO",igrbout)  ! GRIB output: 1=GRIB1; 2=GRIB2
+      print *, 'INPUT is GRIB',igrbin,', OUTPUT is GRIB',igrbout
+
+      IGDNUM=1 ! Temporarily set so GRIB2 code will work
+
       FNAME='fort.  '
 !====================================================================
 !     FHR3 = -99 signals a 6 hour summation requested
@@ -74,17 +98,24 @@
       READ (5,*) FHR1, FHR2,FHR3,FHR4
       print *,' SMARTPRECIP ', FHR1,FHR2,FHR3,FHR4
 
+      MK3P=.FALSE.; MK6P=.FALSE.; MK12P=.FALSE.
       LRD3=.FALSE.;LRD4=.FALSE.
 !==>  Make 3 hour buckets by subtracting the 1st file from the 2nd
       LSUB=.FALSE.
+
+        writE(0,*) 'enter FHR1, FHR2, FHR3, FHR4: ', &
+                          FHR1, FHR2, FHR3, FHR4
+
       LADDSUB=.FALSE.
       IF (FHR1.GT.FHR2) THEN
+        write(0,*) 'subtracting'
         MK3P=.TRUE.
         SHR1=FHR2
         FHR2=FHR1   ! T
         FHR1=SHR1   ! T-3
         IF (FHR3.GT.0) SHR1=FHR3  ! DGEX, T-6
         LSUB=.TRUE.
+        write(0,*) 'reset so SHR1, FHR1, FHR2 are: ', SHR1, FHR1, FHR2
 
 !==>    Set SHR1 to read NAM Accumulated snowfall bucket : 3,6,9 or 12 hr snow
 !         =initial hour for snow bucket (T-3, -6, -9, -12
@@ -92,8 +123,10 @@
 !         NAM has 12 hr snow buckets at 00 and 12 UTC Valid times
           IF (MOD(FHR2,12).EQ. 0.) THEN   
             SHR1=FHR2-12
+        write(0,*) 'SHR1 defined(a): ', SHR1
           ELSE
             SHR1=FHR2-MOD(FHR2,12) 
+        write(0,*) 'SHR1 defined(b): ', SHR1
           ENDIF
         else
           SHR1=FHR2-6
@@ -147,6 +180,8 @@
 !=======================================================
 !  READ INDEX FILE TO GET GRID SPECS 
 !=======================================================
+      if(igrbin .eq. '1')then
+
       CALL RDHDRS(LUGB,LUGI,JPDS,JGDS,IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
 
 ! -== GET PRECIP FIELDS ==-
@@ -298,6 +333,343 @@
 
       ENDIF 
 
+      elseif (igrbin .eq. '2')then
+
+! -== GET SURFACE FIELDS ==-
+
+
+!        allocate(gfld%fld(1200*1200))
+        allocate(gfld%idsect(200))
+        allocate(gfld%igdtmpl(200))
+        allocate(gfld%ipdtmpl(200))
+        allocate(gfld%idrtmpl(200))
+!        allocate(gfld%bmap(1200*1200))
+
+        JIDS=-9999
+        JPDTN=-1
+        JPDT=-9999
+        JGDTN=-1
+        JGDT=-9999
+        UNPACK=.false.
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB
+        CALL BAOPENR(LUGB,FNAME,IRETGB)
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB2
+        CALL BAOPENR(LUGB2,FNAME,IRETGB)
+
+        write(0,*) 'trim(fname): ', trim(fname)
+
+        write(0,*) 'IRETGB on BAOPEN: ', IRETGB
+
+        call getgb2(LUGB2,LUGI2,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+
+        write(0,*) 'IRET from init getgb2 call: ', IRET
+
+        NUMVAL=gfld%ngrdpts
+
+        write(0,*) 'NUMVAL ', NUMVAL
+
+        if (IRET .ne. 0) STOP
+
+        UNPACK=.true.
+
+      ALLOCATE (MASK(NUMVAL),GRID(NUMVAL),STAT=kret)
+      ALLOCATE (APCP1(NUMVAL),CAPCP1(NUMVAL),SNOW1(NUMVAL),STAT=kret)
+      IF(kret.ne.0)THEN
+       WRITE(*,*)'ERROR allocation source location: ',numval
+       STOP
+      END IF
+
+!   PRECIP
+
+        write(0,*) 'have NUMVAL : ', NUMVAL
+
+        write(0,*) 'allocate again?'
+        allocate(gfld%fld(NUMVAL))
+        allocate(gfld%bmap(NUMVAL))
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+        UNPACK=.true.
+
+      print *;print *,'FHR1= ',FHR1, ' READ 1st PRECIP FILE ', LUGB,LUGI
+
+        call getgb2(LUGB,LUGI,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET_EARLY)
+
+        write(0,*) 'IRET from GETGB2: ', IRET_EARLY
+
+        if (IRET_EARLY .ne. 0) THEN
+
+        write(0,*) 'set APCP1 to zero'
+        APCP1=0.
+
+        else
+
+        write(0,*) 'size(APCP1): ', size(APCP1)
+        write(0,*) 'size(gfld%fld): ', size(gfld%fld)
+
+        APCP1=gfld%fld
+
+
+        do K=1,29
+        PDS_RAIN_HOLD_EARLY(K)=gfld%ipdtmpl(K)
+        enddo
+
+        endif
+
+!  SKIP CONVECTIVE PRECIP
+
+!  SNOWFALL
+      J = 0;JPDS = -1;JPDS(3) = IGDNUM
+      JPDS(5) = 065;JPDS(6) = 001
+
+!       if (IARW .eq. 0) then
+
+      JPDS(14) = SHR1  ! t-6 or T-3 accumulated snow
+      JPDS(15) = FHR1  ! T
+      print *,'FHRS ',JPDS(14),JPDS(15),' READ 1st SNOW FILE ', LUGB,LUGI
+        write(0,*) 'SHR1: ', SHR1
+!     if (fhr4.gt.0) JPDS(14)=FHR0
+!       write(0,*) 'JPDS(14) for snow now: ', JPDS(14)
+
+!       endif
+
+        JIDS=-9999
+        JPDTN=1
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+
+        if (IRET .ne. 0) THEN
+
+        write(0,*) 'set SNOW1 to zero'
+        SNOW1=0.
+
+        else
+
+        SNOW1=gfld%fld
+
+!       do K=1,200
+        do K=1,29
+        PDS_SNOW_HOLD_EARLY(K)=gfld%ipdtmpl(K)
+        enddo
+
+        endif
+
+
+!=======================================================
+!  READ 2nd file
+!=======================================================
+
+      print *;print *,'RD 2nd File', FHR2,LUGB2,LUGI2
+
+      ALLOCATE (APCP2(NUMVAL),CAPCP2(NUMVAL),SNOW2(NUMVAL),STAT=kret)
+      IF(kret.ne.0)THEN
+       WRITE(*,*)'ERROR allocation source location: ',numval
+       STOP
+      END IF
+
+!     ACCUMULATED PRECIP
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB2,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        APCP2=gfld%fld
+
+
+!       do K=1,200
+        do K=1,29
+        PDS_RAIN_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
+! SKIP ACCUMULATED CONVECTIVE PRECIP
+
+!     SNOWFALL
+      J = 0;JPDS = -1;JPDS(3) = IGDNUM
+      JPDS(5) = 065;JPDS(6) = 001
+!       if (IARW .eq. 0) then
+      JPDS(14) = FHR1
+      JPDS(15) = FHR2
+!     IF (LSUB) JPDS(14)=FHR3
+      IF (LSUB) JPDS(14)=SHR1  ! T-3 Shouldnt need this ??
+      print *,'FHRS ',JPDS(14),JPDS(15),' READ 2nd SNOW FILE ', LUGB2,LUGI2
+!       endif
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB2,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+
+        SNOW2=gfld%fld
+
+
+!       do K=1,200
+        do K=1,29
+        PDS_SNOW_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
+
+!     IF (FHR4.GT.0 ) THEN
+      IF (LRD3) THEN
+
+!=======================================================
+!  READ 3rd file
+!  For 12 hr precip summations and DGEX 6 hr calculations
+!=======================================================
+!      CALL RDHDRS(LUGB3,LUGI3,JPDS,JGDS,              &
+!                  IGDNUM,IMAX,JMAX,KMAX,NUMVAL)
+
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB3
+        CALL BAOPENR(LUGB3,FNAME,IRETGB)
+
+      ALLOCATE (APCP3(NUMVAL),CAPCP3(NUMVAL),SNOW3(NUMVAL),STAT=kret)
+      IF(kret.ne.0)THEN
+       WRITE(*,*)'ERROR allocation source location: ',numval
+       STOP
+      END IF
+
+!     ACCUMULATED PRECIP
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB3,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        APCP3=gfld%fld
+
+
+!       do K=1,200
+        do K=1,29
+        PDS_RAIN_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
+! SKIP ACCUMULATED CONVECTIVE PRECIP
+
+!     SNOWFALL
+      J = 0 ;JPDS = -1;JPDS(3) = IGDNUM
+      JPDS(5) = 065;JPDS(6) = 001
+!       if (IARW .eq. 0) then
+        JPDS(14) = FHR2 ! T-6
+        JPDS(15) = FHR3 ! T-3
+      print *,'FHR3=',FHR3,' READ 3rd PRECIP FILE ', LUGB3,LUGI3
+!       endif
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB3,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        SNOW3=gfld%fld
+
+
+!       do K=1,200
+        do K=1,29
+        PDS_SNOW_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
+        ENDIF
+!=======================================================
+!  READ 4th file
+!  READ INDEX FILE TO GET GRID SPECS for 4th file (FOR 12 hour NAM precip)
+!=======================================================
+      IF (MK12P .and. LRD4) THEN
+        WRITE(FNAME(6:7),FMT='(I2)')LUGB4
+        CALL BAOPENR(LUGB4,FNAME,IRETGB)
+
+      ALLOCATE (APCP4(NUMVAL),CAPCP4(NUMVAL),SNOW4(NUMVAL),STAT=kret)
+      IF(kret.ne.0)THEN
+       WRITE(*,*)'ERROR allocation source location: ',numval
+       STOP
+      END IF
+!     ACCUMULATED PRECIP
+      J = 0;JPDS = -1;JPDS(3) = IGDNUM
+      JPDS(5) = 061;JPDS(6) = 001
+      JPDS(13) = 1
+      print *;print *,'FHR4=',FHR4,' READ 4th PRECIP FILE ', LUGB4,LUGI4
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB4,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        APCP4=gfld%fld
+
+
+!       do K=1,200
+        do K=1,29
+        PDS_RAIN_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
+! SKIP ACCUMULATED CONVECTIVE PRECIP
+
+!     SNOWFALL
+      J = 0 ;JPDS = -1;JPDS(3) = IGDNUM
+      JPDS(5) = 065;JPDS(6) = 001
+!       if (IARW .eq. 0) then
+        JPDS(14) = FHR3 ! T-3
+        JPDS(15) = FHR4 ! T
+      print *,JPDS(14),JPDS(15),' READ 4th SNOW FILE ', LUGB4,LUGI4
+!       endif
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=13
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(LUGB4,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT, &
+                    UNPACK,K,GFLD,IRET)
+        SNOW4=gfld%fld
+
+
+!       do K=1,200
+        do K=1,29
+        PDS_SNOW_HOLD(K)=gfld%ipdtmpl(K)
+        enddo
+
+
+      ENDIF
+
+      else
+        print*,'igrbin is not defined...stopping'
+        stop
+      endif ! igrbin
 !=======================================================
 !     OUTPUT 3, 6 or 12 hr PRECIP BUCKETS
 !=======================================================
@@ -307,10 +679,14 @@
        STOP
       END IF
 
+!! LSUB --> 3 h total
+!! FHR4 > 0 --> 12 h total
+!! nothing --> 6 h total
+
       print *
       IF (LSUB) THEN
        APCPOUT=APCP2-APCP1
-       CAPCPOUT=CAPCP2-CAPCP1
+!      CAPCPOUT=CAPCP2-CAPCP1
        SNOWOUT=SNOW2-SNOW1
        KPDS(14)=FHR1
        KPDS(15)=FHR2
@@ -318,7 +694,7 @@
 
       ELSE
         APCPOUT=APCP2+APCP1
-        CAPCPOUT=CAPCP2+CAPCP1
+!       CAPCPOUT=CAPCP2+CAPCP1
         SNOWOUT=SNOW2+SNOW1
 
 !       6 hr precip 
@@ -327,14 +703,66 @@
           IF (LADDSUB) THEN   
             KPDS(15)=FHR1
             APCPOUT=APCP3+(APCP2-APCP1)
-            CAPCOUT=CAPC3+(CAPC2-CAPC1)
+!           CAPCOUT=CAPC3+(CAPC2-CAPC1)
             SNOWOUT=SNOW3+(SNOW2-SNOW1)
             print *, 'OUTPUT 06 HR PRECIP: ADDSUB',FHR1,FHR2,FHR3,maxval(apcpout)
           ELSE
             KPDS(15)=FHR2
             print *,'OUTPUT 6 HR PRECIP: ADD', FHR1,FHR2,maxval(apcpout)
           ENDIF
-        ENDIF
+! Grib2
+        if (IRET_EARLY .ne. 0) then
+          gfld%ipdtmpl=PDS_RAIN_HOLD
+        else
+          gfld%ipdtmpl=PDS_RAIN_HOLD_EARLY
+        endif
+
+!       gfld%ipdtmpl(9)=ihrs1
+        gfld%ipdtmpl(9)=1
+
+        do J=16,21
+        gfld%ipdtmpl(J)=PDS_RAIN_HOLD(J)
+        enddo
+
+        gfld%ipdtmpl(22)=1
+
+! default as a 6 h accumulation?
+        write(0,*) 'here FHR3, FHR2: ', FHR3, FHR2
+
+!        if (LSUB) then
+
+        gfld%ipdtmpl(27)= FHR1-FHR2 ! 3 hr accum
+        gfld%ipdtmpl(9)=FHR2 
+
+
+!! use of (27) here looks wrong!
+
+!        write(0,*) 'gfld%ipdtmpl(27) bef: ', &
+!                    gfld%ipdtmpl(27)
+!        write(0,*) 'gfld%ipdtmpl(9) bef: ', &
+!                    gfld%ipdtmpl(9)
+
+!        gfld%ipdtmpl(27)=3
+!        gfld%ipdtmpl(9)=FHR1
+
+       write(0,*) 'gfld%ipdtmpl(27) aft: ', &
+                   gfld%ipdtmpl(27)
+       write(0,*) 'gfld%ipdtmpl(9) aft: ', &
+                   gfld%ipdtmpl(9)
+
+!        endif
+
+        gfld%ipdtmpl(27)=6
+        gfld%ipdtmpl(9)=FHR3
+
+       write(0,*) 'gfld%ipdtmpl(27) aft: ', &
+                   gfld%ipdtmpl(27)
+       write(0,*) 'gfld%ipdtmpl(9) aft: ', &
+                   gfld%ipdtmpl(9)
+
+! Grib2
+
+         ENDIF
  
 !       12 hr precip
         IF (MK12P) THEN
@@ -342,12 +770,15 @@
           KPDS(15)=FHR4
           IF (LRD4) THEN
             APCPOUT=APCPOUT+APCP3+APCP4
-            CAPCPOUT=CAPCPOUT+CAPCP3+CAPCP4
+!           CAPCPOUT=CAPCPOUT+CAPCP3+CAPCP4
             SNOWOUT=SNOWOUT+SNOW3+SNOW4
             print *, ' OUTPUT 12 HR PRECIP: ADD 4 ',FHR1,FHR2,FHR3,FHR4,maxval(apcpout)
           ELSE
             print *, ' OUTPUT 12 HR PRECIP: ADD 2 ',FHR1,FHR2,maxval(apcpout)
           ENDIF
+        gfld%ipdtmpl(27)=12
+        gfld%ipdtmpl(9)=SHR1
+
         ENDIF
 
       ENDIF
@@ -357,21 +788,34 @@
       print *, 'writing precip', KPDS(5),KPDS(14),KPDS(15),LUGB5,MAXVAL(APCPOUT)
       WRITE(FNAME(6:7),FMT='(I2)')LUGB5
       CALL BAOPEN(LUGB5,FNAME,IRETGB)
+      if(igrbout .eq. '1')then
       CALL PUTGB(LUGB5,NUMVAL,KPDS,KGDS,MASK,APCPOUT,IRET)
+      elseif (igrbout .eq. '2')then
+        gfld%ipdtmpl(2)=8
+        gfld%fld=APCPOUT      
+      call putgb2(LUGB5,GFLD,IRET)
+      endif
       CALL BACLOSE(LUGB5,IRET)
 
-      KPDS(5)=63
-      print *, 'writing CAPCP', KPDS(5),KPDS(14),KPDS(15),LUGB6 , MAXVAL(CAPCPOUT)
-      WRITE(FNAME(6:7),FMT='(I2)')LUGB6
-      CALL BAOPEN(LUGB6,FNAME,IRET)
-      CALL PUTGB(LUGB6,NUMVAL,KPDS,KGDS,MASK,CAPCPOUT,IRET)
-      CALL BACLOSE(LUGB6,IRET)
+! SKIP CONVECTIVE ACCUMULATED PRECIP
+!     KPDS(5)=63
+!     print *, 'writing CAPCP', KPDS(5),KPDS(14),KPDS(15),LUGB6 , MAXVAL(CAPCPOUT)
+!     WRITE(FNAME(6:7),FMT='(I2)')LUGB6
+!     CALL BAOPEN(LUGB6,FNAME,IRET)
+!     CALL PUTGB(LUGB6,NUMVAL,KPDS,KGDS,MASK,CAPCPOUT,IRET)
+!     CALL BACLOSE(LUGB6,IRET)
 
       KPDS(5)=65
       print *, 'writing SNOW', KPDS(5),KPDS(14),KPDS(15),LUGB7, MAXVAL(SNOWOUT)
       WRITE(FNAME(6:7),FMT='(I2)')LUGB7
       CALL BAOPEN(LUGB7,FNAME,IRET)
+      if(igrbout .eq. '1')then
       CALL PUTGB(LUGB7,NUMVAL,KPDS,KGDS,MASK,SNOWOUT,IRET)
+      else
+      gfld%ipdtmpl(2)=13
+      gfld%fld=SNOWOUT
+      call putgb2(LUGB7,GFLD,IRET)
+      endif
       CALL BACLOSE(LUGB7,IRET)
 
       STOP
