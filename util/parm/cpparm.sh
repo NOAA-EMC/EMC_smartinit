4679e1e4d0003987fefe864c797239168aea06d8
annette.gibbs@noaa.gov <annette.gibbs@noaa.gov>
2015-06-17 23:30:32 +0000

    Add GRIB2 parm files

---
+++ util/parm/cpparm.sh
@@ -0,0 +1,23 @@
+#!/bin/ksh
+
+typeset -Z2 fhr
+mdl=nam
+endhrs=84
+
+for rgn in pr conus;do
+  case $rgn in 
+     pr)grd=254; ogrd=195;;
+     conus)grd=188; ogrd=184;;
+  esac
+  for cctp in on off;do
+    fhr=00
+    while [ $fhr -le $endhrs ];do
+      ffold=grib2_awp${mdl}dng${rgn}${cctp}f${fhr}.${ogrd}
+      ffnew=grib2_awp${mdl}dng${rgn}${cctp}f${fhr}.${grd}
+      echo $ffold
+      echo $ffnew
+      cp $ffold $ffnew
+      ((fhr=fhr+3))
+    done
+  done
+done
