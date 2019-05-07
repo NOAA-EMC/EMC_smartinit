a6084ac7a07ab06b42ea35c1f9633cdbe4d79873
Annette.Gibbs <Annette.Gibbs@noaa.gov>
2019-05-07 13:52:23 +0000

    This commit adds the NAM Smartinit changes associated with the Dell Transition.  The changes are described in the docs/README file.

--- sorc/build_smartprecip_g2.sh
+++ sorc/build_smartprecip_g2.sh
@@ -3,9 +3,15 @@ set -x
 BASE=`pwd`
 export BASE
 
-. /usrx/local/Modules/default/init/ksh
+#. /usrx/local/Modules/default/init/ksh
+ . /usrx/local/prod/lmod/lmod/init/profile
 module purge
-module load $BASE/../modulefiles/SMARTINIT/v4.2.0
+module load EnvVars/1.0.2
+moduledir=`dirname $(readlink -f ../modulefiles/SMARTINIT)`
+module use ${moduledir}
+module load SMARTINIT/v4.2.1
+#module use $BASE/../modulefiles/SMARTINIT
+#module load $BASE/../modulefiles/SMARTINIT/v4.2.0
 module list
 
 sleep 1
@@ -15,6 +21,6 @@ sleep 1
 cd ${BASE}/smartprecip_g2.fd
 make clean
 make 
-make clean
+#make clean
 
 ##############################
