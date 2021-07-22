12b53383093f59b8d3bffbe83a2a81430cb6c6f7
Annette.Gibbs <Annette.Gibbs@noaa.gov>
2021-07-22 14:41:48 +0000

    Merging smartinit.v4.3.1 into the master.  Task #90055 - Update RTMA consensus land/sea mask and terrain files for CONUS.
    Squashed commit of the following:
    
    commit 22eacc376c3cd6d1381ae25e7042fac38ad48052
    Author: Annette.Gibbs <Annette.Gibbs@noaa.gov>
    Date:   Wed Jul 21 20:50:28 2021 +0000
    
        This commit adds changes associated with the NAM Smartinit production failure on 20210611 due to a NAM Coldstart.  Replaced OVERDATEG2 with WGRIB2 and COMIN with COM_IN to change date of grib2 file.  Also, made changes to let job abort instead of silent exit.
    
    commit 9791c2ad7462e5175d9a32ed5d271575627d3e24
    Author: Annette.Gibbs <Annette.Gibbs@noaa.gov>
    Date:   Thu May 13 16:53:55 2021 +0000
    
        This commit adds changes for the RFC associated with RTMA/URMA v2.8.7.  The RTMA CONUS land/sea mask and terrain around the Great Salt Lake are updated to better match the lake's current shape.

--- sorc/build_smartprecip_g2.sh
+++ sorc/build_smartprecip_g2.sh
@@ -8,7 +8,7 @@ module purge
 module load EnvVars/1.0.2
 moduledir=`dirname $(readlink -f ../modulefiles/SMARTINIT)`
 module use ${moduledir}
-module load SMARTINIT/v4.3.0
+module load SMARTINIT/v4.3.1
 module list
 
 sleep 1
